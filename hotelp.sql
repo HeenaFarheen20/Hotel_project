88e829107ac1f8da1b7e052c00794e64fcef49d5
Heena Farheen <111047003+HeenaFarheen20@users.noreply.github.com>
2023-03-06 07:31:04 +0200

    Update hotelp.sql

--- hotelp.sql
+++ hotelp.sql
@@ -1,45 +1,30 @@
+Use project;
 
-with recursive hotelp(
-hotel, 
-is_canceled, 
-lead_time,  
-arrival_date_year,  
-arrival_date_month,  
-arrival_date_week_number,  
-arrival_date_day_of_month,  
-stays_in_weekend_nights,  
-stays_in_week_nights,  
-adults,  
-children,  
-babies,  
-meal,  
-country,  
-market_segment,  
-distribution_channel,  
-is_repeated_guest,  
-previous_cancellations,  
-previous_bookings_not_canceled, 
-reserved_room_type, 
-assigned_room_type,  
-booking_changes,  
-deposit_type,  
-agent, 
-company,  
-days_in_waiting_list,  
-customer_type, 
-adr, 
-required_car_parking_spaces,  
-total_of_special_requests,  
-reservation_status, 
-reservation_status_date)
+* -- To get the answer of how  much revenue generated in each hotel type in three diffirent years.
+
+With hotels
 as (select * from project.2018
 union
 select * from project.2019
 union
 select * from project.2020)
-select * from hotelp
-left join project.market_segment
-on hotelp.market_segment = market_segment.market_segment
-left join project.meal_cost
-on hotelp.meal = meal_cost.meal ;
+select 
+arrival_date_year, hotel,
+round(sum((stays_in_week_nights + stays_in_weekend_nights)*adr),2) as Revenue 
+FROM hotels 
+group by arrival_date_year, hotel;
 
+*-- Get the answer how much discount associated and how much cost earned by meals in each hotel type.
+
+WITH HOTELS AS(
+SELECT * FROM project.`2018`
+UNION
+SELECT * FROM project.`2019`
+UNION
+SELECT * FROM project.`2020`)
+
+SELECT * from hotels
+left join project.market_segment
+on hotels.market_segment = market_segment.market_segment
+left join project.meal_cost
+on meal_cost.meal = hotels.meal;
